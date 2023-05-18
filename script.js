7c2ae38d79655828ed204f0573875ab99efbef1c
ngntrgduc <trungducnguyen1511@gmail.com>
2023-05-18 10:07:26 +0700

    Add more utils
    Increase/decrease button, relax button

--- script.js
+++ script.js
@@ -7,22 +7,32 @@ function padding(number) {
     return number.toString().padStart(2, '0');
 }
 
-function display(minutes, seconds) {
+function display(minutes, seconds=0) {
     let time = padding(minutes) + ":"+ padding(seconds);
     document.getElementById("time").innerHTML = time;
     document.title = time; // Update title
 }
 
-function start() {
+function alertMessage() {
+    alert('<(￣︶￣)> Done');
+    clearInterval(interval); 
+}
+
+function start(loop=false) {
     if (!started) {
         minutes = startMinutes;
         started = true;
         let seconds = 60;
         interval = setInterval(function() {
             if(minutes <= 0 && seconds == 60) {
-                alert('<(￣︶￣)> Done');
-                clearInterval(interval); 
-                return;
+                if (!loop) {
+                    alertMessage();
+                    return;
+                } else {
+                    clearInterval(interval); 
+                    startMinutes = 30;
+                    start(loop=true)
+                }
             }
             if (seconds == 60) { minutes -= 1; }
             seconds--;
@@ -33,9 +43,40 @@ function start() {
 }
 
 function reset() {
-    display(startMinutes, 0)
+    startMinutes = 30;
+    display(startMinutes)
     clearInterval(interval);
     started = false;
 }
+
+function relax() {
+    if (started) clearInterval(interval);
+    started = false;
+    startMinutes = 5;
+    display(startMinutes);
+}
+
+// function loop() {
+//     clearInterval(interval);
+//     started = false;
+//     startMinutes = 30;
+//     start(loop=true)
+//     display(startMinutes)   
+// }
+
+function increaseTime() {
+    if (!started) {
+        startMinutes += 5;
+        display(startMinutes);
+    }
+}
+
+function decreaseTime() {
+    if (!started) {
+        startMinutes -= 5;
+        display(startMinutes);
+    }
+}
+
 
 reset() // Initial
